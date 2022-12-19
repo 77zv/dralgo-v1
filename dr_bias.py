eef71c2b201f9e6192ca07fa523ed7b3af599ee9
Tony <antonylbrussolo@gmail.com>
2022-12-19 18:21:03 -0500

    oanda_api call is complete: 1. changed the column names of the dr_low and dr_high dataframes
    up next:
    1. find 50% of the range between the high and lows

--- dr_bias.py
+++ dr_bias.py
@@ -16,11 +16,13 @@ def bias(df: DataFrame) -> tuple[DataFrame, DataFrame, DataFrame]:
     # Select only the rows between 9:30 and 10:30
     df_range: DataFrame = df.set_index('time').between_time('9:30', '10:30')
 
-
     # Grouping the dataframe by business day and then taking the max and min of each group.
-    df_high: DataFrame = df_range.groupby(pd.Grouper(freq='B')).max()
-    df_low: DataFrame = df_range.groupby(pd.Grouper(freq='B')).min()
+    df_high: DataFrame = df_range.groupby(pd.Grouper(freq='B')).max().rename(
+        columns={'mid_o': 'dr_high_o', 'mid_h': 'dr_high_h', 'mid_l': 'dr_high_l', 'mid_c': 'dr_high_c'})
+
+    df_low: DataFrame = df_range.groupby(pd.Grouper(freq='B')).min().rename(
+        columns={'mid_o': 'dr_low_o', 'mid_h': 'dr_low_h', 'mid_l': 'dr_low_l', 'mid_c': 'dr_low_c'})
+
 
 
     return df_high, df_low, df_range
-
