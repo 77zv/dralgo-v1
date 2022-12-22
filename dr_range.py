b8ce9d257afc933d197bbacc43d8cc5340939f37
tonyy.lb <antonylbrussolo@gmail.com>
2022-12-22 14:37:44 -0500

    Changes: 1. made a new dataframe with only the dr_high_h, dr_low_low, and dr_equilibrium to act officially as an indicator
    up next:
    1. append the dr dataframe to the candlestick dataframe.
    2. check if price retraces to equilibrium
    3. if price retraces into equilibrium, set a "sl" below/above the low/high, and set a "tp" 1 std of the

--- dr_range.py
+++ dr_range.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pandas import DataFrame
 
 
-def bias(df: DataFrame) -> tuple[DataFrame, DataFrame, DataFrame]:
+def bias(df: DataFrame) -> DataFrame:
     """
     It takes a dataframe as input, and returns the highest high and lowest low of the dataframe
 
@@ -13,42 +13,20 @@ def bias(df: DataFrame) -> tuple[DataFrame, DataFrame, DataFrame]:
     # Select only the rows between 9:30 and 10:30
     df_range: DataFrame = df.set_index('time').between_time('9:30', '10:30')
 
-    # Grouping the dataframe by business day and then taking the max and min of each group.
-    df_high: DataFrame = df_range.groupby(pd.Grouper(freq='B')).max().rename(
+    # Taking the max and min of the dataframe for each day and renaming the columns.
+    df_high: DataFrame = df_range.groupby(df_range.index.date).max().rename(
         columns={'mid_o': 'dr_high_o', 'mid_h': 'dr_high_h', 'mid_l': 'dr_high_l', 'mid_c': 'dr_high_c'})
-    df_low: DataFrame = df_range.groupby(pd.Grouper(freq='B')).min().rename(
+    df_low: DataFrame = df_range.groupby(df_range.index.date).min().rename(
         columns={'mid_o': 'dr_low_o', 'mid_h': 'dr_low_h', 'mid_l': 'dr_low_l', 'mid_c': 'dr_low_c'})
 
     dr_range = pd.concat([df_low, df_high], axis=1)
     # adds column that is 50% of the high and low
     dr_range['dr_equilibrium'] = (dr_range['dr_high_h'] + dr_range['dr_low_l']) / 2
-    # adds column that is 61.8% of the range between the high and low (OTE)
-    dr_range["dr_ote"] = dr_range['dr_high_h'] - (dr_range['dr_high_h'] + dr_range['dr_low_l']) * 0.618
 
-    return df_low, df_high, dr_range
+    dr_indicator = {}
+    for date, row in dr_range.iterrows():
+        dr_indicator[date] = {'dr_high_h': row['dr_high_h'], 'dr_low_l': row['dr_low_l'], 'dr_equilibrium': row['dr_equilibrium']}
 
+    dr_indicator = pd.DataFrame.from_dict(dr_indicator).T
 
-def evaluate_bias(df: DataFrame) -> None:
-    # Assume that df is a DataFrame containing the data you want to check against
-
-    # Create an empty column called 'above_below'
-    df['above_below'] = ""
-
-    # Iterate over each row in the dataframe
-    for index, row in df.iterrows():
-        # Get the close price for the current day
-        close_price = row['mid_c']
-
-        # Get the high and low levels for the current day
-        dr_high_high = row['dr_high_h']
-        dr_low_low = row['dr_low_l']
-
-        if close_price > dr_high_high:
-            # Set the 'above_below' column to 'above' if the close price is above dr_high_high
-            df.loc[index, 'above_below'] = 'above'
-        elif close_price < dr_low_low:
-            # Set the 'above_below' column to 'below' if the close price is below dr_low_low
-            df.loc[index, 'above_below'] = 'below'
-        else:
-            # Set the 'above_below' column to 'between' if the close price is between dr_high_high and dr_low_low
-            df.loc[index, 'above_below'] = 'between'
+    return dr_indicator
--- main.py
+++ main.py
@@ -8,11 +8,8 @@ if __name__ == "__main__":
     print(SPX.df.tail())
     print(SPX.df.dtypes)
 
-    pd.reset_option('max_columns')
-    dr_low, dr_high, dr_range = bias(SPX.df)
-    print(f"The Defining Range is defined by the following candles \n{dr_range.tail(13)}\n")
-    print(f"The highs of the last 3 Defining Ranges \n{dr_high.tail(3)}\n")
-    print(f"The lows of the last 3 Defining Ranges \n{dr_low.tail(3)}\n")
+    dr_indicator = bias(SPX.df)
+    print(f"The Defining Range is defined by the following candles \n{dr_indicator}\n")
 
-    # SPX.df = pd.concat([SPX.df, dr_range], axis=1)
-    # print(f"Historic Data combined with the Defining Range: \n{SPX.df.tail(26)}\n")
+    # iterate through dr_range and adds the dr_high_h and dr_low_l to a dictionary with the key being the date
+
