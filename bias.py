3bf4372c031fbb7614f6d56d38fa07ade7f84be5
tonyy.lb <antonylbrussolo@gmail.com>
2022-12-23 21:25:46 -0500

    Changes: 1. removed candles before 9:30, determined the bias for each day according to the DR
    up next:
    0. make a second dataframe with daily candles, and check to see if the days close is above/below the DR, and if that aligns with the bias
    1. check if price retraces to equilibrium
    2. if price retraces into equilibrium, set a "sl" below/above the low/high, and set a "tp" 1 std of the

--- bias.py
+++ bias.py
@@ -10,7 +10,18 @@ def evaluate_dr_bias(row):
     :param row: the row of the dataframe that is being iterated over
     :return: A string
     """
-    if row['mid_c'] > row['dr_high_h'] and row.name.time() > datetime.strptime('10:30', '%H:%M').time():
+
+    def evaluate_time(df_row):
+        """
+        It takes a row of a dataframe as input, and returns True if the time of the row is greater than 10:30, and False
+        otherwise
+
+        :param df_row: the row of the dataframe that is being evaluated
+        :return: A boolean value
+        """
+        return df_row.name.time() > datetime.strptime('10:30', '%H:%M').time()
+
+    if row['mid_c'] > row['dr_high_h'] and evaluate_time(row):
         return 'Above DR High'
-    elif row['mid_c'] < row['dr_low_l'] and row.name.time() > datetime.strptime('10:30', '%H:%M').time():
+    elif row['mid_c'] < row['dr_low_l'] and evaluate_time(row):
         return 'Below DR Low'
