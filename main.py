899b8d17f5d7732b4260df27a84cf42446892686
77zv <antonylbrussolo@gmail.com>
2023-08-14 21:41:17 -0400

    not sure what updates

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ weeks = 100
 run_backtest(backtesting_dr, 10000, 0.02, 1, weeks)
 
 pd.set_option('display.max_rows', 1000)
-#
-# spx: OandaAPI = OandaAPI()
-#
-# start_dates, end_dates = get_weeks(1)
-# data: DataFrame = spx.create_data("SPX500_USD", "M15", 4000, start_dates[0], end_dates[0])
-#
-# fair_value_gap_strategy(data)
+
+spx: OandaAPI = OandaAPI()
+
+start_dates, end_dates = get_weeks(1)
+data: DataFrame = spx.create_data("SPX500_USD", "M15", 4000, start_dates[0], end_dates[0])
+
+fair_value_gap_strategy(data)
